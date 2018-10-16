80c0a328b87c6a8f100e00434792d9ee5eb02714
Bojan <bojan@ancon.se>
2018-10-16 22:59:33 +0200

    Add project files.

---
+++ src/MediaS.API/Controllers/ImagesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaS.API.Controllers {
+    [ApiVersion("1.0")]
+    [Route("api/media/[controller]")]
+    [ApiController]
+    public class ImagesController : BaseController{
+
+    }
+}
